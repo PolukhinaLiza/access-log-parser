41ffac509489c66dbc3003aa15ff4497c24961bc
Nazarova Elizaveta <polukhina.liza.01@mail.ru>
2025-05-27 14:59:19 +0300

    Добавлен бесконечный цикл по проверке пути к файлу

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
@@ -5,5 +6,23 @@ public class Main {
         System.out.println("Введите текст и нажмите <Enter>: ");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста: " + text.length());
+
+        // Задание по теме "Циклы"
+        int fileNumber = 1;
+        while (true){
+            System.out.println("Введите путь к файлу");
+            String path = new Scanner(System.in).nextLine();
+            File file = new File(path);
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
+
+            if (!fileExists || isDirectory) {
+                System.out.println("Вы не верно ввели путь к файлу");
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + fileNumber++ );
+        }
+
     }
 }
