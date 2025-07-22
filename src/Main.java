0a0b67dbdfce39f69a47ce8af3edebdb1ab08b88
Nazarova Elizaveta <polukhinaei@pochtabank.ru>
2025-07-22 21:13:42 +0300

    добавлено сравнение объектов и поиск доли обращения популярных ботов

--- src/Main.java
+++ src/Main.java
@@ -26,10 +26,11 @@ public class Main {
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + fileNumber++ );
 
-            //Задание по обработке исключений
+            //Задание по обработке исключений и сравнении объектов
             int totalLines = 0;
-            int minLength = Integer.MAX_VALUE;
-            int maxLength = 0;
+            int totalRequests = 0;
+            int requestsYandexBot = 0;
+            int requestsGoogleBot = 0;
             try {
                 FileReader fileReader = new FileReader(path);
                 BufferedReader reader =
@@ -43,12 +44,33 @@ public class Main {
                         throw new LineTooLongException("Строка превышает 1024 символа, номер строки: " + totalLines);
                     }
 
-                    if (length < minLength) minLength = length;
-                    if (length > maxLength) maxLength = length;
+                    String userAgent = extractUserAgent(line);
+                    if (userAgent==null) continue;
+                    totalRequests++;
+                    String fragment = extractFragmentUserAgent(userAgent);
+                    if (fragment==null) continue;
+                    String botName = getBotName(fragment);
+                    if (botName== null) continue;
+
+                    if (botName.equals("YandexBot")) {
+                        requestsYandexBot++;
+                    }
+                    if (botName.equals("Googlebot")) {
+                        requestsGoogleBot++;
+                    }
+
+
                 }
+                double yandexBot = (double)requestsYandexBot/totalRequests * 100;
+                double googleBot = (double)requestsGoogleBot/totalRequests * 100;
+
+
                 System.out.println("Всего строк в файле: " + totalLines);
-                System.out.println("Длина самой длинной строки в файле: " + maxLength);
-                System.out.println("Длина самой короткой строки в файле: " + (minLength == Integer.MAX_VALUE ? 0 : minLength));
+                System.out.println("Доля запросов от YandexBot: " + yandexBot);
+                System.out.println("Доля запросов от Googlebot " + googleBot);
+                System.out.println(requestsGoogleBot);
+                System.out.println(requestsYandexBot);
+                System.out.println(totalRequests);
             } catch (LineTooLongException ex) {
                 System.out.println(ex.getMessage());
             }
@@ -57,5 +79,42 @@ public class Main {
             }
         }
 
+    }
+
+    //метод для поиска части user-agent
+    private static String extractUserAgent(String line) {
+        int lastQuoteIndex = line.lastIndexOf('"');
+        if (lastQuoteIndex != -1) {
+            int secondLastQuoteIndex = line.lastIndexOf('"', lastQuoteIndex - 1);
+            if (secondLastQuoteIndex != -1) {
+                return line.substring(secondLastQuoteIndex + 1, lastQuoteIndex).trim();
+            }
+        }
+        return null;
+    }
+
+    //метод для поиска фрагмента
+    private static String extractFragmentUserAgent(String userAgent) {
+        int firstOpenBracketIndex = userAgent.indexOf('(');
+        int firstCloseBracketIndex = userAgent.indexOf(')', firstOpenBracketIndex);
+
+        if (firstOpenBracketIndex != -1 && firstCloseBracketIndex != -1) {
+            String firstBrackets = userAgent.substring(firstOpenBracketIndex + 1, firstCloseBracketIndex);
+            String[] parts = firstBrackets.split(";");
+            if (parts.length >= 2) {
+                return  parts[1].trim(); // второй элемент после разделения без пробелов в начале и конце
+            }
+        }
+        return null; //
+    }
+
+    //метод для поиска названия
+    private static String getBotName(String fragment) {
+        int index = fragment.indexOf('/');
+
+        if (index != -1) {
+            return fragment.substring(0, index);
+        }
+        return null; //
     }
 }
