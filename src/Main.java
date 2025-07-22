256d2eb505c6b9dc2e4c58ad7fb112b11ec99c02
Nazarova Elizaveta <polukhinaei@pochtabank.ru>
2025-07-22 17:52:24 +0300

    добавлена обработка файла по строкам

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.io.File;
+import java.io.*;
 import java.util.Scanner;
 
 public class Main {
@@ -25,6 +25,36 @@ public class Main {
             }
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + fileNumber++ );
+
+            //Задание по обработке исключений
+            int totalLines = 0;
+            int minLength = Integer.MAX_VALUE;
+            int maxLength = 0;
+            try {
+                FileReader fileReader = new FileReader(path);
+                BufferedReader reader =
+                        new BufferedReader(fileReader);
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    totalLines++;
+                    int length = line.length();
+
+                    if (length > 1024) {
+                        throw new LineTooLongException("Строка превышает 1024 символа, номер строки: " + totalLines);
+                    }
+
+                    if (length < minLength) minLength = length;
+                    if (length > maxLength) maxLength = length;
+                }
+                System.out.println("Всего строк в файле: " + totalLines);
+                System.out.println("Длина самой длинной строки в файле: " + maxLength);
+                System.out.println("Длина самой короткой строки в файле: " + (minLength == Integer.MAX_VALUE ? 0 : minLength));
+            } catch (LineTooLongException ex) {
+                System.out.println(ex.getMessage());
+            }
+            catch (IOException ex) {
+                ex.printStackTrace();
+            }
         }
 
     }
